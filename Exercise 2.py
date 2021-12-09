87c2ef0b06f3e1a13d487e2977465f1d999713b5
Asawari Upadhye <asawari.upadhye@nice.com>
2021-12-09 19:07:57 +0530

    Exercise commit

---
+++ Exercise 2.py
@@ -0,0 +1,10 @@
+## Problem Statement : Ask a user their weight(in pounds), convert it to kilograms and
+##out put it to the terminal
+
+weight_in_pounds = input("What is your weight in lbs ")
+weight_in_kgs = float(weight_in_pounds) * 0.45
+
+print("Your weight in kilograms is " + str(weight_in_kgs))
+
+## you see two conversions in this code, one after accepting the string input and the other
+##when you are printing the float calculation as a string to the terminal at the end.
